e6ea27c402759d34a95d830f7102735f95a0fb94
Matej <mtjkovac@gmail.com>
2021-10-19 09:01:52 +0200

    make items list, finish "zobrazenie zoznamu" exercise

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from '../item';
+import {ITEMS} from '../mock-items';
 
 @Component({
   selector: 'app-items',
@@ -12,10 +13,16 @@ export class ItemsComponent implements OnInit {
     id: 1,
     name: 'axe',
     price: 150
-  }
+  };
+  items = ITEMS;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectedItem?: Item;
+  onSelect(item: Item): void {
+    this.selectedItem = item;
   }
 
 }
---
+++ src/app/mock-items.ts
@@ -0,0 +1,8 @@
+import {Item} from './item';
+
+export const ITEMS: Item[] = [
+    { id: 21, name: 'Axe', price: 10 },
+    { id: 22, name: 'Knife', price: 20 },
+    { id: 23, name: 'Gun', price: 30 },
+    { id: 24, name: 'Pickaxe', price: 40 },
+]
