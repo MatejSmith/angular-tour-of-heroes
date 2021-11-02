176960bcc63371037441eef9694cb274957a2324
MatejSmith <mtjkovac@gmail.com>
2021-11-02 09:09:38 +0100

    add hero service + fix ITEMS in mock-heroes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -4,12 +4,12 @@ import {ITEMS} from './mock-items';
 export const HEROES: Hero[] = [
     { id: 11, name: 'Dr Nice', item: [ITEMS[0]]},
     { id: 12, name: 'Narco', item: [ITEMS[1]]},
-    { id: 13, name: 'Bombasto', item: [ITEMS[23]]},
-    { id: 14, name: 'Celeritas', item: [ITEMS[24]]},
-    { id: 15, name: 'Magneta', item: [ITEMS[21]]},
-    { id: 16, name: 'RubberMan', item: [ITEMS[22]]},
-    { id: 17, name: 'Dynama', item: [ITEMS[23]]},
-    { id: 18, name: 'Dr IQ', item: [ITEMS[24]]},
-    { id: 19, name: 'Magma', item: [ITEMS[21]]},
-    { id: 20, name: 'Tornado', item: [ITEMS[22]]}
+    { id: 13, name: 'Bombasto', item: [ITEMS[2]]},
+    { id: 14, name: 'Celeritas', item: [ITEMS[3]]},
+    { id: 15, name: 'Magneta', item: [ITEMS[0]]},
+    { id: 16, name: 'RubberMan', item: [ITEMS[1]]},
+    { id: 17, name: 'Dynama', item: [ITEMS[2]]},
+    { id: 18, name: 'Dr IQ', item: [ITEMS[3]]},
+    { id: 19, name: 'Magma', item: [ITEMS[0]]},
+    { id: 20, name: 'Tornado', item: [ITEMS[1]]}
 ]
