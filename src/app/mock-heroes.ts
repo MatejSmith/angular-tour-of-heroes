a480e042c512fb13c1dccc51f4ca1c5c04e9ae49
Matej Kovac <mtjkovac@gmail.com>
2022-01-23 18:36:13 +0100

    add life and strength for heroes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,14 +2,14 @@ import {Hero} from './hero';
 import {ITEMS} from './mock-items';
 
 export const HEROES: Hero[] = [
-    { id: 11, name: 'Dr Nice', item: [ITEMS[0]], money: 15},
-    { id: 12, name: 'Narco', item: [ITEMS[1]], money: 25},
-    { id: 13, name: 'Bombasto', item: [ITEMS[2]], money: 17},
-    { id: 14, name: 'Celeritas', item: [ITEMS[3]], money: 50},
-    { id: 15, name: 'Magneta', item: [ITEMS[0]], money: 28},
-    { id: 16, name: 'RubberMan', item: [ITEMS[1]], money: 43},
-    { id: 17, name: 'Dynama', item: [ITEMS[2]], money: 70},
-    { id: 18, name: 'Dr IQ', item: [ITEMS[3]], money: 34},
-    { id: 19, name: 'Magma', item: [ITEMS[0]], money: 100},
-    { id: 20, name: 'Tornado', item: [ITEMS[1]], money: 59}
+    { id: 11, name: 'Dr Nice', item: [ITEMS[0]], money: 15, life: 56, strength: 84},
+    { id: 12, name: 'Narco', item: [ITEMS[1]], money: 25, life: 23, strength: 91},
+    { id: 13, name: 'Bombasto', item: [ITEMS[2]], money: 17, life: 86, strength: 16},
+    { id: 14, name: 'Celeritas', item: [ITEMS[3]], money: 50, life: 45, strength: 54},
+    { id: 15, name: 'Magneta', item: [ITEMS[0]], money: 28, life: 34, strength: 74},
+    { id: 16, name: 'RubberMan', item: [ITEMS[1]], money: 43, life: 50, strength: 50},
+    { id: 17, name: 'Dynama', item: [ITEMS[2]], money: 70, life: 71, strength: 29},
+    { id: 18, name: 'Dr IQ', item: [ITEMS[3]], money: 34, life: 22, strength: 78},
+    { id: 19, name: 'Magma', item: [ITEMS[0]], money: 100, life: 60, strength: 40},
+    { id: 20, name: 'Tornado', item: [ITEMS[1]], money: 59, life: 80, strength: 20}
 ]
